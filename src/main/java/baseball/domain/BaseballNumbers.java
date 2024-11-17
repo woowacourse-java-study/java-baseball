2e80b36e06d13710eddffa48ee921f4da34f90c1
이창근 <dlckdrms0517@gmail.com>
2024-11-17 18:05:52 +0900

    feat: BaseballNumbers 도메인 생성

---
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -0,0 +1,35 @@
+package baseball.domain;
+
+import baseball.common.exception.BaseballExceptions;
+import baseball.common.validator.ParamsValidator;
+import baseball.service.numberPicker.NumberPicker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballNumbers {
+	
+	private static final int BASEBALL_NUMBERS_SIZE = 3;
+	
+	private final List<BaseballNumber> baseballNumbers;
+	
+	public BaseballNumbers(List<BaseballNumber> baseballNumbers) {
+		ParamsValidator.validateParamsNotNull(baseballNumbers);
+		validate(baseballNumbers);
+		this.baseballNumbers = baseballNumbers;
+	}
+	
+	private static void validate(List<BaseballNumber> baseballNumbers) {
+		if (baseballNumbers.size() != BASEBALL_NUMBERS_SIZE) {
+			throw BaseballExceptions.ILLEGAL_ARGUMENT.get();
+		}
+	}
+	
+	public static BaseballNumbers generate(NumberPicker numberPicker) {
+		List<BaseballNumber> baseballNumbers = new ArrayList<>();
+		for (int i = 0; i < BASEBALL_NUMBERS_SIZE; i++) {
+			baseballNumbers.add(BaseballNumber.generate(numberPicker));
+		}
+		return new BaseballNumbers(baseballNumbers);
+	}
+}
