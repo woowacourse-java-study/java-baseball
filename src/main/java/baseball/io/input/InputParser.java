3346937ca188b6b4bcd3ca323c7b75025738c2e4
이창근 <dlckdrms0517@gmail.com>
2024-11-18 16:32:27 +0900

    refactor: InputParser의 예외처리를 InputValidator로 옮김에 따라, 예외처리 삭제

--- src/main/java/baseball/io/input/InputParser.java
+++ src/main/java/baseball/io/input/InputParser.java
@@ -1,14 +1,11 @@
 package baseball.io.input;
 
-import baseball.common.exception.BaseballExceptions;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class InputParser {
 	
 	public List<Integer> parseBaseballNumbers(String input) {
-		// TODO : 예외 처리
 		List<Integer> baseballNumbers = new ArrayList<>();
 		for (char ch : input.toCharArray()) {
 			baseballNumbers.add(Integer.parseInt(String.valueOf(ch)));
@@ -17,13 +14,6 @@ public class InputParser {
 	}
 	
 	public boolean parseReplayDecsion(String input) {
-		// TODO : 예외 처리
-		if (input.equals("1")) {
-			return true;
-		}
-		if (input.equals("2")) {
-			return false;
-		}
-		throw BaseballExceptions.ILLEGAL_ARGUMENT.get();
+		return input.equals("1");
 	}
 }
