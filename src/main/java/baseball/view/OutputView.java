703b9290ed4a1a1e21a8bc2cf3e40959e8616f78
023-dev <0_2_3@naver.com>
2024-11-18 21:01:43 +0900

    feat(view): 게임 재시작 프롬프트 출력 메서드 printRestartPrompt 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -15,6 +15,10 @@ public class OutputView {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
+    public void printRestartPrompt() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
     public void printError(String message) {
         System.out.println(ERROR.formatted(message));
     }
