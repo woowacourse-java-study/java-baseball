128ef27ab504675215889f1ecea8816a23a28c44
023-dev <0_2_3@naver.com>
2024-11-18 21:56:31 +0900

    feat(main): GameConfig 초기화 및 게임 시작 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.config.GameConfig;
+import baseball.controller.GameComputer;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameConfig gameConfig = new GameConfig();
+        GameComputer gameComputer = gameConfig.getGameComputer();
+
+        gameComputer.start();
     }
 }
