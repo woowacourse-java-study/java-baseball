ba53a048491fc25d02fb17108e63e91af8c8ca17
023-dev <0_2_3@naver.com>
2024-11-19 03:18:22 +0900

    feat(controller): 에러 메시지 처리를 위한 메서드 error 추가

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -2,45 +2,57 @@ package baseball.controller;
 
 import baseball.model.domain.ComputerNumbers;
 import baseball.model.domain.GameMenu;
-import baseball.model.domain.GameNumbers;
 import baseball.model.domain.PlayerNumbers;
+import baseball.model.domain.Result;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class GameController {
     private final InputView inputView;
     private final OutputView outputView;
-    private final GameNumbers gameNumbers;
 
-    public GameController(InputView inputView, OutputView outputView, GameNumbers gameNumbers) {
+    public GameController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.gameNumbers = gameNumbers;
     }
 
     public void start() {
-        boolean isContinue = true;
-        outputView.printStartMessage();
-        while (isContinue) {
-            play();
-            isContinue = restart();
+        try {
+            boolean isContinue = true;
+            outputView.printStartMessage();
+            while (isContinue) {
+                play();
+                isContinue = restart();
+            }
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
         }
-
     }
 
     private void play() {
         boolean isFinish = false;
-        ComputerNumbers computerNumbers = new ComputerNumbers();
+        ComputerNumbers computerNumbers = init();
         while (!isFinish) {
-            PlayerNumbers playerNumbers = new PlayerNumbers(input());
-            String result = gameNumbers.calculateResult(computerNumbers, playerNumbers);
+            PlayerNumbers playerNumbers =  input();
+            isFinish = result(computerNumbers, playerNumbers);
         }
         finish();
     }
 
-    private String input() {
+    private ComputerNumbers init() {
+        return new ComputerNumbers();
+    }
+
+    private PlayerNumbers input() {
         outputView.printNumberPrompt();
-        return inputView.getNumber();
+        String input = inputView.getNumber();
+        return new PlayerNumbers(input);
+    }
+
+    private boolean result(ComputerNumbers computerNumbers, PlayerNumbers playerNumbers) {
+        Result result = computerNumbers.compare(playerNumbers);
+        outputView.printResultMessage(result.toString());
+        return result.isCorrect();
     }
 
     private boolean restart() {
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -11,6 +11,10 @@ public class OutputView {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
+    public void printResultMessage(String message) {
+        System.out.println(message);
+    }
+
     public void printFinishMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
@@ -19,7 +23,7 @@ public class OutputView {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    public void printError(String message) {
+    public void printErrorMessage(String message) {
         System.out.println(ERROR.formatted(message));
     }
 
