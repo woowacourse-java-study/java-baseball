7592c7a93509f1091571892dc02bda2621ee9ae2
023-dev <0_2_3@naver.com>
2024-11-18 20:54:15 +0900

    feat(view): 에러 메시지 출력 메서드 printError 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,6 +1,7 @@
 package baseball.view;
 
 public class OutputView {
+    private final static String ERROR = "[ERROR]%s";
 
     public void printStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -8,6 +9,10 @@ public class OutputView {
 
     public void printNumberPrompt() {
         System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public void printError(String message) {
+        System.out.println(ERROR.formatted(message));
     }
 
 }
