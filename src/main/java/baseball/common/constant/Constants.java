1232aada48a6d170067e6cb8978fae08bd256330
023-dev <0_2_3@naver.com>
2024-11-19 02:56:28 +0900

    feat(constant): 상수 값 관리를 위한 클래스 Constants 추가

--- src/main/java/baseball/common/constant/Constants.java
+++ src/main/java/baseball/common/constant/Constants.java
@@ -1,7 +1,13 @@
 package baseball.common.constant;
 
 public class Constants {
+    public final static int MIN_NUMBER = 1;
+    public final static int MAX_NUMBER = 9;
+    public final static int NUMBERS_SIZE = 3;
+
 
     public final static String WHITE_SPACE = " ";
-
+    public final static String STRIKE = "%d스트라이커";
+    public final static String BALL = "%d볼";
+    public final static String NOTHING = "낫싱";
 }
