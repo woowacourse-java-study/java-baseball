1e34c4b214cd585745ab2ff94129a05939995513
023-dev <0_2_3@naver.com>
2024-11-19 00:03:43 +0900

    feat(domain): GameNumbers에 대한 검증 로직 및 메서드 parse 추가

--- src/main/java/baseball/model/domain/GameNumbers.java
+++ src/main/java/baseball/model/domain/GameNumbers.java
@@ -2,16 +2,36 @@ package baseball.model.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameNumbers {
     private final List<GameNumber> gameNumbers;
 
     public GameNumbers(String input) {
-        this.gameNumbers = generateGameNumbers(input);
+        this.gameNumbers = parse(input);
     }
 
-    private List<GameNumber> generateGameNumbers(String input) {
+    private List<GameNumber> parse(String input) {
+        List<String> items = List.of(input.split(""));
         List<GameNumber> gameNumbers = new ArrayList<>();
+        for (String item : items) {
+            GameNumber gameNumber = new GameNumber(item);
+            gameNumbers.add(gameNumber);
+        }
+        validate(gameNumbers);
         return gameNumbers;
+    }
+
+    private void validate(List<GameNumber> gameNumbers) {
+        if (gameNumbers.size() != 3) {
+            throw new IllegalArgumentException("숫자 3자리를 입력해야 합니다.");
+        }
+        int uniqueCount = gameNumbers.stream()
+                .map(GameNumber::getNumber)
+                .collect(Collectors.toSet())
+                .size();
+        if (uniqueCount != 3) {
+            throw new IllegalArgumentException("중복된 숫자는 허용되지 않습니다.");
+        }
     }
 }
