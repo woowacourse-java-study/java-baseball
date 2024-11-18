3eb1df73e8549f62551e06160ad35ca9559a2b4a
023-dev <0_2_3@naver.com>
2024-11-19 02:55:10 +0900

    refactor(domain): 오버 엔지니어링 방지를 위한 코드 응집 및 클래스 제거

--- src/main/java/baseball/model/domain/ComputerNumbers.java
+++ src/main/java/baseball/model/domain/ComputerNumbers.java
@@ -1,23 +1,47 @@
 package baseball.model.domain;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import static baseball.common.constant.Constants.MAX_NUMBER;
+import static baseball.common.constant.Constants.MIN_NUMBER;
+import static baseball.common.constant.Constants.NUMBERS_SIZE;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class ComputerNumbers {
-    private final GameNumbers computerNumbers;
+    private final List<Integer> computerNumbers;
 
     public ComputerNumbers() {
-        this.computerNumbers = createComputerNumbers();
+        this.computerNumbers = createRandomNumbers();
     }
 
-    public static GameNumbers createComputerNumbers() {
+    private List<Integer> createRandomNumbers() {
         Set<Integer> randomNumbers = new HashSet<>();
-        while (randomNumbers.size() < 3) {
-            int randomNumber = pickNumberInRange(1, 9);
+        while (randomNumbers.size() < NUMBERS_SIZE) {
+            int randomNumber = pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             randomNumbers.add(randomNumber);
         }
-        return new GameNumbers(randomNumbers);
+        return new ArrayList<>(randomNumbers);
+    }
+
+    public Result compare(PlayerNumbers player) {
+        int strikes = 0;
+        int balls = 0;
+
+        List<Integer> playerNumbers = player.getPlayerNumbers();
+
+        for (int i = 0; i < NUMBERS_SIZE; i++) {
+            boolean isStrike = computerNumbers.get(i).equals(playerNumbers.get(i));
+            boolean isBall = !isStrike&&computerNumbers.contains(playerNumbers.get(i));
+            if (isStrike) {
+                strikes++;
+            }
+            if (isBall) {
+                balls++;
+            }
+        }
+        return new Result(strikes, balls);
     }
 }
--- src/main/java/baseball/model/domain/PlayerNumbers.java
+++ src/main/java/baseball/model/domain/PlayerNumbers.java
@@ -1,9 +1,56 @@
 package baseball.model.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerNumbers {
-    private final GameNumbers playerNumbers;
+    private final List<Integer> playerNumbers;
 
     public PlayerNumbers(String input) {
-        this.playerNumbers = new GameNumbers(input);
+        this.playerNumbers = parse(input);
+    }
+
+    private List<Integer> parse(String input) {
+        List<String> items = List.of(input.split(""));
+        List<Integer> numbers = new ArrayList<>();
+        for (String item : items) {
+            int number = parseNumber(item);
+            numbers.add(number);
+        }
+        validateNumbers(numbers);
+        return numbers;
+    }
+
+    private int parseNumber(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            validateNumber(number);
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값은 숫자여야 합니다.");
+        }
+    }
+
+    private int validateNumber(int number) {
+        if (number < 1 || number > 9) {
+            throw new IllegalArgumentException("숫자는 1에서 9까지의 수만 허용됩니다.");
+        }
+        return number;
+    }
+
+    private void validateNumbers(List<Integer> numbers) {
+        if (numbers.size() != 3) {
+            throw new IllegalArgumentException("숫자 3자리를 입력해야 합니다.");
+        }
+        int uniqueCount = (int) numbers.stream()
+                .distinct()
+                .count();
+        if (uniqueCount != 3) {
+            throw new IllegalArgumentException("중복된 숫자는 허용되지 않습니다.");
+        }
+    }
+
+    public List<Integer> getPlayerNumbers() {
+        return playerNumbers;
     }
 }
