80dd380a0a8780b146a576727be7486897574220
jinhyeon <tiemo0708@naver.com>
2024-11-19 12:54:27 +0900

    feat: 야구게임 숫자 랜덤생성기능

--- src/main/java/baseball/config/Appconfig.java
+++ src/main/java/baseball/config/Appconfig.java
@@ -1,14 +1,25 @@
 package baseball.config;
 
 import baseball.controller.BaseballController;
+import baseball.generator.BaseballNumberGenerator;
+import baseball.generator.BaseballNumberGeneratorImpl;
+import baseball.validation.InputValidation;
+import baseball.validation.InputValidationImpl;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class Appconfig {
     public BaseballController baseballController() {
-        return new BaseballController(inputView(), outputView());
+        return new BaseballController(inputView(), outputView(), inputValidation(),baseballNumberGenerator());
     }
 
+    public InputValidation inputValidation(){
+        return new InputValidationImpl();
+    }
+
+    public BaseballNumberGenerator baseballNumberGenerator(){
+        return new BaseballNumberGeneratorImpl();
+    }
     public InputView inputView() {
         return new InputView();
     }
--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,19 +1,30 @@
 package baseball.controller;
 
+import baseball.generator.BaseballNumberGenerator;
+import baseball.generator.BaseballNumberGeneratorImpl;
+import baseball.validation.InputValidation;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+
+import java.util.List;
 
 public class BaseballController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final InputValidation inputValidationImpl;
+    private final BaseballNumberGenerator baseballNumberGeneratorImpl;
 
-    public BaseballController(InputView inputView, OutputView outputView) {
+
+    public BaseballController(InputView inputView, OutputView outputView, InputValidation inputValidationImpl, BaseballNumberGenerator baseballNumberGeneratorImpl) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.inputValidationImpl = inputValidationImpl;
+        this.baseballNumberGeneratorImpl = baseballNumberGeneratorImpl;
     }
 
     public void start() {
-        inputView.inputBasebalNumber();
+        List<Integer> baseballNumber = baseballNumberGeneratorImpl.generate();
+        inputValidationImpl.vlalidate(inputView.inputBasebalNumber());
 
     }
 }
